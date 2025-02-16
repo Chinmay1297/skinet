d41434576d638811c61426a5572099ddaa7b2ed4
Chinmay Prusty <kanha12297@gmail.com>
2025-02-16 20:22:10 +0530

    Added Entity Framework Core Nugget Packages

---
+++ Infrastructure/Data/StoreContext.cs
@@ -0,0 +1,13 @@
+using System;
+using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data;
+
+public class StoreContext : DbContext
+{
+    public DbSet<Product> Products { get; set; }
+    public StoreContext(DbContextOptions options) : base(options)
+    {
+    }
+}
