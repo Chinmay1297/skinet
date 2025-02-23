8c41a855766b6060d914db8346366c457e59f53d
Chinmay Prusty <kanha12297@gmail.com>
2025-02-23 18:52:10 +0530

    Added Projections to our specification

--- Infrastructure/Data/SpecificationEvaluator.cs
+++ Infrastructure/Data/SpecificationEvaluator.cs
@@ -8,21 +8,46 @@ public class SpecificationEvaluator<T> where T : BaseEntity
 {
     public static IQueryable<T> GetQuery(IQueryable<T> query, ISpecification<T> spec)
     {
-        if(spec.Criteria != null)
+        if (spec.Criteria != null)
         {
             query = query.Where(spec.Criteria);         //x=>x.price == price
         }
 
-        if(spec.OrderBy != null)
+        if (spec.OrderBy != null)
         {
             query = query.OrderBy(spec.OrderBy);
         }
 
-        if(spec.OrderByDescending != null)
+        if (spec.OrderByDescending != null)
         {
             query = query.OrderByDescending(spec.OrderByDescending);
         }
 
         return query;
+    }
+    public static IQueryable<TResult> GetQuery<TSpec, TResult>(IQueryable<T> query, ISpecification<T, TResult> spec)
+    {
+        if (spec.Criteria != null)
+        {
+            query = query.Where(spec.Criteria);         //x=>x.price == price
+        }
+
+        if (spec.OrderBy != null)
+        {
+            query = query.OrderBy(spec.OrderBy);
+        }
+
+        if (spec.OrderByDescending != null)
+        {
+            query = query.OrderByDescending(spec.OrderByDescending);
+        }
+
+        var selectQuery = query as IQueryable<TResult>;
+        if (spec.Select != null)
+        {
+            selectQuery = query.Select(spec.Select);
+        }
+
+        return selectQuery ?? query.Cast<TResult>();
     }
 }
