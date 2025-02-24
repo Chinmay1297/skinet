f41780cc920412e8981165328422f968d6950ee9
Chinmay Prusty <kanha12297@gmail.com>
2025-02-24 19:02:47 +0530

    Added product search to our product specification

--- Core/Specifications/ProductSpecification.cs
+++ Core/Specifications/ProductSpecification.cs
@@ -5,23 +5,24 @@ namespace Core.Specifications;
 
 public class ProductSpecification : BaseSpecification<Product>
 {
-    public ProductSpecification(ProductSpecParams specParams) : base(x=>
+    public ProductSpecification(ProductSpecParams specParams) : base(x =>
+        (string.IsNullOrEmpty(specParams.Search) || x.Name.ToLower().Contains(specParams.Search)) &&
         (specParams.Brands.Count == 0 || specParams.Brands.Contains(x.Brand)) &&
         (specParams.Types.Count == 0 || specParams.Types.Contains(x.Type))
     )
     {
         ApplyPaging(specParams.PageSize * (specParams.PageIndex - 1), specParams.PageSize);
-        
-        switch(specParams.Sort)
+
+        switch (specParams.Sort)
         {
             case "priceAsc":
-                AddOrderBy(p=>p.Price);
+                AddOrderBy(p => p.Price);
                 break;
             case "priceDesc":
-                AddOrderByDescending(p=>p.Price);
+                AddOrderByDescending(p => p.Price);
                 break;
             default:
-                AddOrderBy(p=>p.Name);
+                AddOrderBy(p => p.Name);
                 break;
         }
     }
