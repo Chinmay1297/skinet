9c0ef3009a8f6315dd63a802c2b5bb3fb24dfb99
Chinmay Prusty <kanha12297@gmail.com>
2025-03-12 00:00:51 +0530

    Added the Cart endpoints

---
+++ API/Controllers/CartController.cs
@@ -0,0 +1,39 @@
+using System;
+using Core.Entities;
+using Core.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+public class CartController(ICartService cartService) : BaseApiController
+{
+    [HttpGet]
+    public async Task<ActionResult<ShoppingCart>> GetCartById(string id)
+    {
+        var cart = await cartService.GetCartAsync(id);
+        return Ok(cart ?? new ShoppingCart { Id = id });
+        //we generate the id on client side , then we try and get the cart if it lives in our redis Database
+        //if its absent in db, we send a new cart with the id client sent
+        //then when client updates cart, it will be inserted/updated in redis
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<ShoppingCart>> UpdateCart(ShoppingCart cart)
+    {
+        var updatedCart = await cartService.SetCartAsync(cart);
+
+        if (updatedCart == null) return BadRequest("Problem with cart");
+
+        return updatedCart;
+    }
+
+    [HttpDelete]
+    public async Task<ActionResult> DeleteCart(string id)
+    {
+        var result = await cartService.DeleteCartAsync(id);
+
+        if (!result) return BadRequest("Problem deleting cart");
+
+        return Ok();
+    }
+}
