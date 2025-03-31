d37958a0693547ea5aec05be06046176b8ec7651
Chinmay Prusty <kanha12297@gmail.com>
2025-03-31 18:03:48 +0530

    Added stripe service and installed stripe-js for stripe forms

--- client/src/environments/environment.development.ts
+++ client/src/environments/environment.development.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: 'https://localhost:5001/api/'
+    apiUrl: 'https://localhost:5001/api/',
+    stripePublicKey: 'pk_test_51R8LUsBXZNsKzI8qgQDYYkdKopMSXW8nBJi5ZX990PPLj8uOHmJo1umSl58w6iLZXinA8rf3lJ4lTcwXuXt0hAXX00nD88f0H9'
 };
--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'api/'
+    apiUrl: 'api/',
+    stripePublicKey: 'pk_test_51R8LUsBXZNsKzI8qgQDYYkdKopMSXW8nBJi5ZX990PPLj8uOHmJo1umSl58w6iLZXinA8rf3lJ4lTcwXuXt0hAXX00nD88f0H9'
 };
