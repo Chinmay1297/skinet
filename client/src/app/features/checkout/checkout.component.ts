e366873959ac284b967f7c0e99c0e0f39c89057b
Chinmay Prusty <kanha12297@gmail.com>
2025-04-01 00:06:39 +0530

    Saving the address change as default address

--- client/src/app/features/checkout/checkout.component.ts
+++ client/src/app/features/checkout/checkout.component.ts
@@ -1,11 +1,16 @@
-import { Component, inject, OnDestroy, OnInit } from '@angular/core';
-import {MatStepperModule} from '@angular/material/stepper';
+import { Component, EventEmitter, inject, OnDestroy, OnInit } from '@angular/core';
+import { MatStepperModule } from '@angular/material/stepper';
 import { OrderSummaryComponent } from "../../shared/components/order-summary/order-summary.component";
 import { MatButton } from '@angular/material/button';
+import { MatCheckboxChange, MatCheckboxModule } from '@angular/material/checkbox';
 import { RouterLink } from '@angular/router';
 import { StripeService } from '../../core/services/stripe.service';
 import { StripeAddressElement } from '@stripe/stripe-js';
 import { SnackbarService } from '../../core/services/snackbar.service';
+import { StepperSelectionEvent } from '@angular/cdk/stepper';
+import { Address } from '../../shared/models/user';
+import { firstValueFrom } from 'rxjs';
+import { AccountService } from '../../core/services/account.service';
 
 @Component({
   selector: 'app-checkout',
@@ -14,7 +19,8 @@ import { SnackbarService } from '../../core/services/snackbar.service';
     OrderSummaryComponent,
     MatStepperModule,
     MatButton,
-    RouterLink
+    RouterLink,
+    MatCheckboxModule
   ],
   templateUrl: './checkout.component.html',
   styleUrl: './checkout.component.scss'
@@ -22,16 +28,47 @@ import { SnackbarService } from '../../core/services/snackbar.service';
 export class CheckoutComponent implements OnInit, OnDestroy {
   private stripeService = inject(StripeService);
   private snackbar = inject(SnackbarService);
+  private accountService = inject(AccountService);
   addressElement?: StripeAddressElement;
+  saveAddress = false;
 
   async ngOnInit() {
-    try{
+    try {
       this.addressElement = await this.stripeService.createAddressElement();
       this.addressElement.mount("#address-element");
     }
-    catch(error: any){
+    catch (error: any) {
       this.snackbar.error(error.message);
     }
+  }
+
+  async onStepChange(event: StepperSelectionEvent) {
+    if (event.selectedIndex === 1) {
+      if (this.saveAddress) {
+        const address = await this.getAddressFromStripeAddress();
+        address && firstValueFrom(this.accountService.updateAddress(address));
+      }
+    }
+  }
+  private async getAddressFromStripeAddress(): Promise<Address | null> {
+    const result = await this.addressElement?.getValue();
+    const address = result?.value.address;
+
+    if (address) {
+      return {
+        line1: address.line1,
+        line2: address.line2 || "",
+        city: address.city,
+        country: address.country,
+        state: address.state,
+        postalCode: address.postal_code,
+      }
+    }
+    return null;
+  }
+
+  onSaveAddressCheckboxChange(event: MatCheckboxChange) {
+    this.saveAddress = event.checked;
   }
 
   ngOnDestroy() {
